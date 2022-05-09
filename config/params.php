42882fb68be43f01e4385fecce7ced40cf2b1f18
andrew-causelof <akozlov@rocketmail.com>
2022-05-09 23:19:59 +0300

    config params and getter/seter for them

---
+++ config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'admin_email' => 'admin@causelof.ru',
+    'shop_name' => 'Sfinks e-shop',
+    'pagination' => 3,
+    'smtp_host' => 'smtp.beget.com',
+    'smtp_port' => '2525',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'admin@causelof.ru',
+    'smtp_password' => '3&AbUlOF'
+];
