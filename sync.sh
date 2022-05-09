bc445c8fd30d03e20a40916f82e0eaf38ca9e8fd
andrew-causelof <akozlov@rocketmail.com>
2022-05-09 21:54:56 +0300

    first settings for laravel

---
+++ sync.sh
@@ -0,0 +1,2 @@
+rsync --progress -rvht --delete-after  cauself9_main@cauself9.beget.tech:~/sfinks.causelof.ru/* ./
+rsync --progress -rvht --delete-after  cauself9_main@cauself9.beget.tech:~/sfinks.causelof.ru/.[^.]* ./
